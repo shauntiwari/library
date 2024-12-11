f0fdd8e35377490f79c1141331ca308e76bb31b0
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-12-11 17:16:57 -0500

    Added logic to display books

--- main.js
+++ main.js
@@ -8,15 +8,31 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-function addBookToLibrary(title, author){
+function addBookToLibrary(title, author, pages, read){
     //add one or more Books to the Library array
-    const bookToAdd = new Book(this.title, this.author);
+    const bookToAdd = new Book(title, author, pages, read);
     myLibrary.push(bookToAdd);
 }
 
 function displayBooks(){
     //loops through the myLibrary array and displays each book on the page
-    for(bookObj in myLibrary){
-        // document.getElementById("container").appendChild(bookObj);
+    for(let i = 0; i < myLibrary.length; i++){
+        const bookDiv = document.createElement("div");
+        const readStatus = myLibrary[i].read ? "read" : "unread";
+        bookDiv.textContent = `${myLibrary[i].title} | ${myLibrary[i].author} | ${myLibrary[i].pages} pages | ${readStatus}`;
+        bookDiv.id = `item ${i}`;
+        document.getElementById("container").appendChild(bookDiv);
     }
 }
+
+// Test logic by adding some books to array, then display books
+addBookToLibrary('Book1', 'Author1', 200, true);
+addBookToLibrary('Book2', 'Author2', 300, false);
+addBookToLibrary('Book3', 'Author3', 160, false);
+addBookToLibrary('Book4', 'Author4', 240, true);
+
+displayBooks();
+
+// Test for removing an item, logic to be used when creating delete buttons
+const delDiv = document.getElementById('item 2');
+delDiv.remove();
